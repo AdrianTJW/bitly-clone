96e18218a3d0a615968677c12075a1d2fcfbf5df
Next <nextacademy@email.com>
2015-12-22 02:21:47 -0800

    Challenge 2 incomplete

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -2,25 +2,27 @@ require_relative '../models/url'
 # require 'byebug'
 
 get '/' do
-	@url = Url.last #assigning Url.last to @urls, so you dont run model in the view folder later.
+	@url = Url.all #assigning Url.all to @urls, so you dont run model in the view folder later.
   erb :"static/index"
 end
 
 post '/urls' do #create a new url
 	x = Url.new(longurl: params[:long_url])
-
-	if x.save
-		redirect to '/'
-	else
-		@error = "url is invalid"
-		redirect to '/'
-	end
+	x.shorten
+	x.clickcount = 0
+	x.save
+	redirect to '/'
 end
 
 get '/:short_url' do #redirect to appropriate "long" URL
 	a = Url.find_by(shorturl: params[:short_url])
+	a.clickcount += 1
+	# byebug
+	a.save
 	redirect a.longurl
 end
+
+
 
 # Another method to save
 # def save
